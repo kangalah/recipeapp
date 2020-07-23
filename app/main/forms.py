8e41640169af05e0bec5d41d1a5ef27a711b7784
sheready <csstlw@gmail.com>
2020-07-23 13:30:51 +0300

    login,signout and sign in route

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,23 +1,35 @@
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, BooleanField
+
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import InputRequired, Email, Length, Required
 
-class RecipeForm(FlaskForm):
+# class RecipeForm(FlaskForm):
     
-    title = StringField('Recipe Title', validators=[Required()])
+#     title = StringField('Recipe Title', validators=[Required()])
 
-    description = StringField('Recipe Description', validators=[Required()])
+#     description = StringField('Recipe Description', validators=[Required()])
 
-    ingredients = TextAreaField('Ingredients')
+#     ingredients = TextAreaField('Ingredients')
 
-    submit = SubmitField('Submit')
+#     submit = SubmitField('Submit')
 
-class ReviewForm(FlaskForm):
+# class ReviewForm(FlaskForm):
     
-    review = TextAreaField('Recipe Review')
+#     review = TextAreaField('Recipe Review')
 
-    submit = SubmitField('Submit')
+#     submit = SubmitField('Submit')
 
-class EditRecipe(FlaskForm):
+# class EditRecipe(FlaskForm):
     
-    submit = SubmitField('Edit Recipe')
+#     submit = SubmitField('Edit Recipe')
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min = 4, max = 15)])
+    password = PasswordField('password', validators = [InputRequired(), Length(min = 8, max = 80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
+    password = PasswordField('password', validators = [InputRequired(), Length(min = 8, max = 80)])
